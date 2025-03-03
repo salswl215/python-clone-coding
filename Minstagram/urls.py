8c99b0cf729065978dec3bfd0acd3be97fea2083
salswl215 <lyn7666@gmail.com>
2025-03-03 15:50:48 +0900

    [Add] show feed list from db

--- Minstagram/urls.py
+++ Minstagram/urls.py
@@ -18,8 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import Main
+from content.views import Main
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-path('main/', Main.as_view())
+    path('main/', Main.as_view())
 ]
--- content/views.py
+++ content/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
-# Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+
+        return render(request, 'minstagram/main.html', context=dict(feeds=feed_list))
