532d4d57809f900bac3dd27abc72129ba5fa2a33
salswl215 <lyn7666@gmail.com>
2025-03-03 20:13:09 +0900

    [Add] feat: upload feed

--- Minstagram/urls.py
+++ Minstagram/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from .views import Main
-from content.views import Main
+from django.conf import settings
+from django.conf.urls.static import static
+from content.views import Main, UploadFeed
+from .settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('main/', Main.as_view())
+    path('', Main.as_view()),
+    path('content/upload', UploadFeed.as_view())
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
