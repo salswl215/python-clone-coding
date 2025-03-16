1bd66282ea29b48d9d38db1fa37be5044c360976
salswl215 <lyn7666@gmail.com>
2025-03-16 17:37:00 +0900

    [Add] feed main page comment/like/bookmark features added

--- content/views.py
+++ content/views.py
@@ -3,14 +3,13 @@ from rest_framework.views import APIView
 from content.models import Feed
 from rest_framework.response import Response
 import os
-from .models import Feed
+from .models import Feed, Reply, Like, Bookmark
 from user.models import User
 from Minstagram.settings import MEDIA_ROOT
 from uuid import uuid4
 
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id')
 
         # print('로그인한 사용자 : ', request.session['email'])
         email = request.session.get('email', None)
@@ -18,10 +17,37 @@ class Main(APIView):
         if email is None:
             return render(request, 'user/login.html')
 
-        user = User.objects.filter(email = email).first()
+        user = User.objects.filter(email=email).first()
 
         if user is None:
             return render(request, 'user/login.html')
+
+        feed_object_list = Feed.objects.all().order_by('-id')
+        feed_list = []
+
+        for feed in feed_object_list:
+            user = User.objects.filter(email=feed.email).first()
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
+            reply_list = []
+            for reply in reply_object_list:
+                reply_user = User.objects.filter(email=reply.email).first()
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       nickname=reply_user.nickname))
+
+            like_count=Like.objects.filter(feed_id=feed.id, is_like=True).count()
+            is_liked=Like.objects.filter(feed_id=feed.id, email=email, is_like=True).exists()
+            is_marked=Bookmark.objects.filter(feed_id=feed.id, email=email, is_marked=True).exists()
+
+            feed_list.append(dict(id=feed.id,
+                                  image=feed.image,
+                                  content=feed.content,
+                                  like_count=like_count,
+                                  profile_image=user.profile_image,
+                                  nickname=user.nickname,
+                                  reply_list=reply_list,
+                                  is_liked=is_liked,
+                                  is_marked=is_marked
+                                  ))
 
         return render(request, 'minstagram/main.html', context=dict(feeds=feed_list, user = user))
 
@@ -37,10 +63,9 @@ class UploadFeed(APIView):
 
         content = request.data.get('content')
         image = uuid_name
-        profile_image = request.data.get('profile_image')
-        user_id = request.data.get('user_id')
+        email = request.session.get('email', None)
 
-        Feed.objects.create(content=content, image=image, profile_image=profile_image, user_id=user_id, like_count=0)
+        Feed.objects.create(content=content, image=image, email=email, like_count=0)
 
         return Response(status=200)
 
@@ -57,3 +82,54 @@ class Profile(APIView):
             return render(request, 'user/login.html')
 
         return render(request, 'content/profile.html', context = dict(user=user))
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
+
+        return Response(status=200)
+class ToggleLike(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        favorite_text = request.data.get('favorite_text', True)
+
+        if favorite_text == 'favorite_border':
+            is_like = True
+        else:
+            is_like = False
+        email = request.session.get('email', None)
+
+        like = Like.objects.filter(feed_id=feed_id, email=email).first()
+
+        if like:
+            like.is_like = is_like
+            like.save()
+        else:
+            Like.objects.create(feed_id=feed_id, is_like=is_like, email=email)
+
+        return Response(status=200)
+
+class ToggleBookmark(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        bookmark_text = request.data.get('bookmark_text', True)
+        print(bookmark_text)
+        if bookmark_text == 'bookmark_border':
+            is_marked = True
+        else:
+            is_marked = False
+        email = request.session.get('email', None)
+
+        bookmark = Bookmark.objects.filter(feed_id=feed_id, email=email).first()
+
+        if bookmark:
+            bookmark.is_marked = is_marked
+            bookmark.save()
+        else:
+            Bookmark.objects.create(feed_id=feed_id, is_marked=is_marked, email=email)
+
+        return Response(status=200)
